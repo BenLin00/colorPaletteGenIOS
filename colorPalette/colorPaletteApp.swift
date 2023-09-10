d4993e453920e5877992c9bd95ae1e9d744b7bc8
Ben Lin <blin2@snap.com>
2023-09-09 22:22:33 -0400

    Initial Commit

---
+++ colorPalette/colorPaletteApp.swift
@@ -0,0 +1,12 @@
+// Copyright © 2023 Snap, Inc. All rights reserved.
+
+import SwiftUI
+
+@main
+struct colorPaletteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
